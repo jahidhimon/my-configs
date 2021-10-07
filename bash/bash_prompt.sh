2cfc45746d327bd4753c95081dc76875fea9d3d5
Jahid Hasan Imon <jahidhasanimonn@gmail.com>
2021-10-07 19:46:35 +0600

    bash_prompt add

---
+++ bash/bash_prompt.sh
@@ -0,0 +1,37 @@
+
+#Regular text color
+BLACK='\[\e[0;30m\]'
+#Bold text color
+BBLACK='\[\e[1;30m\]'
+#background color
+BGBLACK='\[\e[40m\]'
+RED='\[\e[0;31m\]'
+BRED='\[\e[1;31m\]'
+BGRED='\[\e[41m\]'
+GREEN='\[\e[0;32m\]'
+BGREEN='\[\033[38;5;40m\]'
+BGGREEN='\[\e[1;32m\]'
+YELLOW='\[\e[0;33m\]'
+BYELLOW='\[\e[1;33m\]'
+BGYELLOW='\[\e[1;33m\]'
+BLUE='\[\e[0;34m\]'
+BBLUE='\[\e[1;34m\]'
+BGBLUE='\[\e[1;34m\]'
+MAGENTA='\[\e[0;35m\]'
+BMAGENTA='\[\e[1;35m\]'
+BGMAGENTA='\[\e[1;35m\]'
+CYAN='\[\e[0;36m\]'
+BCYAN='\[\e[1;36m\]'
+BGCYAN='\[\e[1;36m\]'
+WHITE='\[\e[0;37m\]'
+BWHITE='\[\e[1;37m\]'
+BGWHITE='\[\e[1;37m\]'
+RESET="\[\017\]"
+NORMAL="\[\033[0m\]"
+RED="\[\033[31;1m\]"
+GLAMBDA="${BGREEN}λ${NORMAL}"
+RLAMBDA="${RED}λ${NORMAL}"
+SELECT="if [ \$? = 0 ]; then echo \"${GLAMBDA}\"; else echo \"${RLAMBDA}\"; fi"
+
+# Throw it all together 
+PS1="${RESET}${GYELLOW}[${BCYAN}\W${NORMAL}${GYELLOW}] \`${SELECT}\` ${NORMAL}"
